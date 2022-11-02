0af533f9db0405244a32a09035eb8d2f8783fc44
Felipe Freitas <felipefreitasdev@gmail.com>
2022-11-02 18:22:04 -0300

    functions complete

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::io;
 
 fn show_options() {
-    println!("=== CALCULADORA DE FÍSICA ===
+    println!("=== CALCULADORA DE CINEMÁTICA ===
 (1) - Velocidade Escalar Média
 (2) - Aceleração Escalar Média
 (3) - Movimento Uniforme
@@ -13,8 +13,8 @@ fn show_options() {
     match option {
         1 => velocity(),
         2 => acceleration(),
-        3 => println!("MU"),
-        4 => println!("MUV"),
+        3 => uniform_motion(),
+        4 => uniformly_varied_motion(),
         5 => kmh_to_ms(),
         6 => ms_to_kmh(),
         _ => println!("Falha na leitura!"),
@@ -40,6 +40,7 @@ fn read_number() -> f64 {
 }
 
 fn acceleration() {
+    println!("\nA = Δv / Δt\n");
     println!("Qual a velocidade INICIAL em m/s?");
     let v_0: f64 = read_number();
     println!("Qual a velocidade FINAL em m/s?");
@@ -57,6 +58,7 @@ A = {} m/s²", delta_v, delta_t, a);
 }
 
 fn velocity() {
+    println!("\nV = Δs / Δt\n");
     println!("Qual a distância percorrida em metros?");
     let delta_s: f64 = read_number();
     println!("Qual o tempo percorrido em segundos?");
@@ -65,6 +67,38 @@ fn velocity() {
     println!("V = Δs / Δt
 V = {} m / {} s
 V = {} m/s", delta_s, delta_t, velocity);
+}
+
+fn uniform_motion() {
+    println!("\nS = S_0 + v.t\n");
+    print!("Qual a posição inicial em metros?");
+    let s_0: f64 = read_number();
+    println!("Qual a velocidade em m/s?");
+    let v: f64 = read_number();
+    println!("Qual foi o tempo percorrido em segundos?");
+    let t: f64 = read_number();
+    let s: f64 = s_0 + v * t;
+    println!("S = S_0 + v.t
+S = {} m + {} m/s * {} s
+S = {} m + {} m
+S = {} m", s_0, v, t, s_0, (v * t), s);
+}
+
+fn uniformly_varied_motion() {
+    println!("\nS = S_0 + vt + at²\n");
+    println!("Qual a posição inicial em metros?");
+    let s_0: f64 = read_number();
+    println!("Qual a velocidade inicial em m/s?");
+    let v_0: f64 = read_number();
+    println!("Qual foi o tempo percorrido em segundos?");
+    let t: f64 = read_number();
+    println!("Qual foi a aceleração em m/s²?");
+    let a: f64 = read_number();
+    let s: f64 = s_0 + v_0 * t + a * (t * t);
+    println!("S = S_0 + vt + at²");
+    println!("S = {} m + {} m/s * {} s + {} m/s² * {} s²
+S = {} m + {} m + {} m
+S = {} m", s_0, v_0, t, a, (t * t), s_0, (v_0 * t), (a * t * t), s);
 }
 
 fn kmh_to_ms() {
